7d10f422b98b5b915fce772b7fc86fc11f986745
zwewaiyanhtet99 <zwehtet.nksoftwarehouse@gmail.com>
2025-06-12 14:38:27 +0630

    source code added

---
+++ SimplePushNotification/Controllers/NotificationController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SimplePushNotification.Models;
+
+namespace SimplePushNotification.Controllers
+{
+    [Route("api/notification")]
+    [ApiController]
+    public class NotificationController : ControllerBase
+    {
+        private readonly INotificationService _notificationService;
+        public NotificationController(INotificationService notificationService)
+        {
+            _notificationService = notificationService;
+        }
+
+        [Route("send")]
+        [HttpPost]
+        public async Task<IActionResult> SendNotification(NotificationModel notificationModel)
+        {
+            var result = await _notificationService.SendNotification(notificationModel);
+            return Ok(result);
+        }
+    }
+}
